daa6062b3d70465c4bea63fe0ecf9206b51a9c05
kalom60 <39136564+kalom60@users.noreply.github.com>
2022-07-06 21:19:19 +0300

    add docstring

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -6,15 +6,15 @@ from functools import wraps
 
 
 def access(method: Callable) -> Callable:
-    """"""
+    """Implementing an expiring web cache and tracker"""
     @wraps(method)
     def count(url: str) -> str:
-        """"""
+        """track how many times a particular URL was accessed"""
         redis_client = redis.Redis()
         redis_client.incr(f'count:{url}')
         cached = redis_client.get(f'cached:{url}')
         if cached:
-            print(cached.decode('utf-8'))
+            return cached.decode('utf-8')
         res = method(url)
         redis_client.setex(f'cached:{url}', 10, res)
         return res
@@ -23,7 +23,7 @@ def access(method: Callable) -> Callable:
 
 @access
 def get_page(url: str) -> str:
-    """"""
+    """send request to url"""
     return requests.get(url).text
 
 
