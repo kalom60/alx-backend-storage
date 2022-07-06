c2463f5e735ee5547c625253e039fe0e0106de9c
kalom60 <39136564+kalom60@users.noreply.github.com>
2022-07-06 21:16:50 +0300

    fix error

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,32 +1,29 @@
 #!/usr/bin/env python3
-"""Implementing an expiring web cache and tracker"""
 import redis
 import requests
 from typing import Callable
 from functools import wraps
 
 
-redis_client = redis.Redis()
-
-
 def access(method: Callable) -> Callable:
-    """track how many times a particular URL was accessed"""
+    """"""
     @wraps(method)
     def count(url: str) -> str:
-        """count how many times a request has been made"""
+        """"""
+        redis_client = redis.Redis()
         redis_client.incr(f'count:{url}')
-        if redis_client.get(f'count:{url}'):
-            print((redis_client.get(f'count:{url}')).decode('utf8'))
-        result = method(url)
-        redis_client.set(f'count:{url}', 1)
-        redis_client.setex(f'result:{url}', 10, result)
-        print((redis_client.get(f'count:{url}')).decode('utf8'))
+        cached = redis_client.get(f'cached:{url}')
+        if cached:
+            print(cached.decode('utf-8'))
+        res = method(url)
+        redis_client.setex(f'cached:{url}', 10, res)
+        return res
     return count
 
 
 @access
 def get_page(url: str) -> str:
-    """call a request"""
+    """"""
     return requests.get(url).text
 
 
