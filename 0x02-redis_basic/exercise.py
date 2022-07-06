ffefa838325a835cab3fae101fef54a968f73704
kalom60 <39136564+kalom60@users.noreply.github.com>
2022-07-06 16:46:56 +0300

    get value from redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """Writing strings to Redis"""
 import redis
-import uuid
-from typing import Union
+from uuid import uuid4
+from typing import Union, Callable
 
 
 class Cache:
@@ -14,6 +14,20 @@ class Cache:
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """write string data with random key"""
-        key = str(uuid.uuid4())
+        key = str(uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Callable = None) ->
+    Union[str, bytes, int, float]:
+        """Get Data from redis"""
+        data = self._redis.get(key)
+        return fn(data) if fn is not None else data
+
+    def get_str(self, key: str) -> str:
+        """get string value from redis"""
+        return self.get(key, lambda x: x.decode('utf-8'))
+
+    def get_int(self, key: str) -> int:
+        """get int value from redis"""
+        return self.get(key, lambda x: int(x))
