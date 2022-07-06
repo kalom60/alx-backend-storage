098eb4664f8843821ab3ca47c6ef109c06670117
kalom60 <39136564+kalom60@users.noreply.github.com>
2022-07-06 20:56:16 +0300

    create result key

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -6,22 +6,32 @@ from typing import Callable
 from functools import wraps
 
 
+redis_client = redis.Redis()
+
+
 def access(method: Callable) -> Callable:
     """track how many times a particular URL was accessed"""
     @wraps(method)
     def count(url: str) -> str:
         """count how many times a request has been made"""
-        redis_client = redis.Redis()
+        redis_client.incr(f'count:{url}')
         if redis_client.get(f'count:{url}'):
-            redis_client.incr(f'count:{url}')
-            return (redis_client.get(f'count:{url}')).decode('utf8')
-        method(url)
-        redis_client.setex(f'count:{url}', 10, 1)
-        return (redis_client.get(f'count:{url}')).decode('utf8')
+            print((redis_client.get(f'count:{url}')).decode('utf8'))
+        result = method(url)
+        redis_client.set(f'count:{url}', 1)
+        redis_client.setex(f'result:{url}', 10, result)
+        print((redis_client.get(f'count:{url}')).decode('utf8'))
     return count
 
 
 @access
 def get_page(url: str) -> str:
     """call a request"""
-    return requests.get(url)
+    return requests.get(url).text
+
+
+get_page('http://slowwly.robertomurray.co.uk')
+get_page('http://slowwly.robertomurray.co.uk')
+get_page('http://slowwly.robertomurray.co.uk')
+get_page('http://slowwly.robertomurray.co.uk')
+get_page('http://slowwly.robertomurray.co.uk')
